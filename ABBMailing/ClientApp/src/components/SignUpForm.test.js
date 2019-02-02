5d8f73f30df9b6a386d8b2960cd2ecae81cc0dd8
pw94 <pawelmwinnicki@gmail.com>
2019-02-02 23:25:57 +0100

    added js tests

--- ABBMailing/ClientApp/src/components/SignUpForm.test.js
+++ ABBMailing/ClientApp/src/components/SignUpForm.test.js
@@ -2,24 +2,17 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { shallow, mount, render } from 'enzyme';
 import { expect } from 'chai';
-import { Form } from 'reactstrap';
+import { Alert, Form } from 'reactstrap';
 import { SignUpForm } from './SignUpForm';
 
 
 describe('SignUpForm', function () {
   beforeEach(() => {
-    this.topics = JSON.parse('[{ "id": 1, "name": "Sport" }, { "id": 2, "name": "Health" }]');    
-  });
-
-  afterEach(() => {
-    if (typeof fetch.resetMocks === 'function') {
-      fetch.resetMocks()
-    }
-  });
-  
-  it('renders without crashing', () => {
+    this.topics = JSON.parse('[{ "id": 1, "name": "Sport" }, { "id": 2, "name": "Health" }]');
     fetch = mockFetch(this.topics);
+  });
 
+  it('renders without crashing', () => {
     render(
       <MemoryRouter>
         <SignUpForm />
@@ -27,51 +20,72 @@ describe('SignUpForm', function () {
   });
 
   it('fetches topics list during the rendering', () => {
-    fetch = mockFetch(this.topics);
-    
     mount(
       <MemoryRouter>
         <SignUpForm />
       </MemoryRouter>);
-    
+
     expect(fetch.mock.calls[0][0]).to.be.equal('api/Topics/List');
   });
-  
-  it('has topics checkboxes and email input', async () => {
-    fetch = mockFetch(this.topics);
 
-    const wrapper = shallow(<SignUpForm />);
+  it('has topics checkboxes and email input', async () => {
+    const component = shallow(<SignUpForm />);
     await Promise.resolve();
 
-    expect(wrapper.find('[type="checkbox"]')).to.have.lengthOf(this.topics.length);
-    expect(wrapper.find('[type="email"]')).to.have.lengthOf(1);
+    expect(component.find('[type="checkbox"]')).to.have.lengthOf(this.topics.length);
+    expect(component.find('[type="email"]')).to.have.lengthOf(1);
   });
 
   it('sends POST when inserted email and selected at least one category', async () => {
-    fetch = mockFetch(this.topics);
-    const wrapper = shallow(<SignUpForm history={[]} />);
+    const component = shallow(<SignUpForm history={[]} />);
     await Promise.resolve();
 
-    wrapper.find('[type="email"]').simulate('change', { target: { value: 'user@server.com' } });
-    const checkboxValue = wrapper.find('[type="checkbox"]').get(0).props.value;
-    wrapper.find('[type="checkbox"]').first().simulate('change', { target: { checked: true, value: checkboxValue } });
-    wrapper.find(Form).simulate('submit', { preventDefault: jest.fn() });
+    const userEmail = 'user@server.com';
+    component.find('[type="email"]').simulate('change', { target: { value: userEmail } });
+    const checkboxValue = component.find('[type="checkbox"]').get(0).props.value;
+    component.find('[type="checkbox"]').first().simulate('change', { target: { checked: true, value: checkboxValue } });
+    component.find(Form).simulate('submit', { preventDefault: jest.fn() });
 
     expect(fetch.mock.calls.length).to.be.equal(2);
     expect(fetch.mock.calls[1][0]).to.be.equal('api/Addresses');
     expect(fetch.mock.calls[1][1].method).to.be.equal('POST');
+    expect(JSON.parse(fetch.mock.calls[1][1].body)).to.be.eql({ topics: [checkboxValue], email: userEmail });
   });
-  
-  it('shows alert and does not send POST when inserted incorrect email address', () => {
-    
+
+  it('shows alert and does not send POST when did not change values', async () => {
+    const component = shallow(<SignUpForm />);
+    await Promise.resolve();
+
+    component.find(Form).simulate('submit', { preventDefault: jest.fn() });
+
+    expect(fetch.mock.calls.length).to.be.equal(1);
+    expect(component.find(Alert).length).to.be.equal(2);
   });
-  
-  it('shows alert and does not send POST when did not choose any category', () => {
-    
+
+  it('shows alert and does not send POST when inserted incorrect email address', async () => {
+    const component = shallow(<SignUpForm />);
+    await Promise.resolve();
+
+    const userEmail = 'incorrect.email';
+    component.find('[type="email"]').simulate('change', { target: { value: userEmail } });
+    const checkboxValue = component.find('[type="checkbox"]').get(0).props.value;
+    component.find('[type="checkbox"]').first().simulate('change', { target: { checked: true, value: checkboxValue } });
+    component.find(Form).simulate('submit', { preventDefault: jest.fn() });
+
+    expect(fetch.mock.calls.length).to.be.equal(1);
+    expect(component.find(Alert).length).to.be.equal(1);
   });
-  
-  it('hides alert when inserted correct email address after the incorrect one', () => {
-    
+
+  it('shows alert and does not send POST when did not choose any category', async () => {
+    const component = shallow(<SignUpForm />);
+    await Promise.resolve();
+
+    const userEmail = 'user@server.com';
+    component.find('[type="email"]').simulate('change', { target: { value: userEmail } });
+    component.find(Form).simulate('submit', { preventDefault: jest.fn() });
+
+    expect(fetch.mock.calls.length).to.be.equal(1);
+    expect(component.find(Alert).length).to.be.equal(1);
   });
 });
 
